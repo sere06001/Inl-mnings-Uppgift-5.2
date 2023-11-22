ae4643d26695051887bf5b60dd42023ea7d80a6a
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 09:53:54 +0100

    Add project files.

---
+++ Inlämnings Uppgift .2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal = int.Parse(Console.ReadLine());
+            int[] allaTal = { tal, 24, 2 };
+            Console.WriteLine(allaTal[0]+" " + allaTal[1]+" " + allaTal[2]);
+            Console.ReadKey();
+        }
+    }
+}
